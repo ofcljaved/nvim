c0486b1314771fada65cf22d8ac8d68e2f44c1fe
ofcljaved <javedofficialme@gmail.com>
2024-09-19 20:23:06 +0530

    feat: add health check

--- lua/captainjacksparrow/health.lua
+++ lua/captainjacksparrow/health.lua
@@ -1,10 +1,3 @@
---[[
---
--- This file is not required for your own configuration,
--- but helps people determine if their system is setup correctly.
---
---]]
-
 local check_version = function()
   local verstr = tostring(vim.version())
   if not vim.version.ge then
@@ -35,13 +28,7 @@ end
 
 return {
   check = function()
-    vim.health.start 'kickstart.nvim'
-
-    vim.health.info [[NOTE: Not every warning is a 'must-fix' in `:checkhealth`
-
-  Fix only warnings for plugins and languages you intend to use.
-    Mason will give warnings for languages that are not installed.
-    You do not need to install, unless you want to use those languages!]]
+    vim.health.start 'captainjacksparrow.nvim'
 
     local uv = vim.uv or vim.loop
     vim.health.info('System Information: ' .. vim.inspect(uv.os_uname()))
